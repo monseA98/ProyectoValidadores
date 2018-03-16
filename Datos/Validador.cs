9dc5c42f6c26653def213a85decd807c9819123f
monseA98 <s16120262@alumnos.itsur.edu.mx>
2018-03-15 21:56:22 -0600

    validadores

---
+++ Datos/Validador.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Validador
+    {
+        //ENTEROS
+        public Boolean esEntero(String entero)
+        {
+            int ne;
+            if (int.TryParse(entero, out ne))
+            {
+                if (entero.Contains(',') || entero.Contains('.'))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else {
+                return false;
+            }
+           
+        }
+
+        //DECIMALES
+        public Boolean esDecimal(String decim)
+        {
+            Decimal nd;
+            if (Decimal.TryParse(decim, out nd))
+            {
+                if (decim.Contains(',') || decim.Contains('.'))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        //EDADES
+        public Boolean esEdad(String edad) {
+            int nedad;
+            if (int.TryParse(edad, out nedad))
+            {
+                if (nedad>0 && nedad<=110)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Vista/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vista
+{
+    public partial class Form1 : Form
+    {
+
+        Datos.Validador o = new Datos.Validador();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ENTERO
+            String valido = "";
+            String inval = "";
+            if (o.esEntero(txtEntero.Text)){
+                valido += "Entero\n";
+            }
+            else {
+                inval += "Entero\n";
+            }
+            //DECIMAL
+            if (o.esDecimal(txtDecimal.Text)){
+                valido += "Decimal\n";
+            }
+            else
+            {
+                inval += "Decimal\n";
+            }
+            //EDAD
+            if (o.esEdad(txtEdad.Text)){
+                valido += "Edad\n";
+            }
+            else
+            {
+                inval += "Edad\n";
+            }
+
+            MessageBox.Show("Validos:\n"+valido+"Invalidos:\n"+inval);
+        }
+    }
+}
